8daec33383459137b45239285de0c9eeafa96004
Bruno Celestino <brunoocelestino@gmail.com>
2025-06-17 08:53:27 -0400

    options binding visuals added
    visuals added but not working

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -6,12 +6,24 @@ using UnityEngine.InputSystem;
 
 public class GameInput : MonoBehaviour
 {
-    public static GameInput instance { get; private set; }
+  public static GameInput instance { get; private set; }
 
   private PlayerInputActions playerInputActions;
   public event EventHandler OnInteractAction;
   public event EventHandler OnInteractAlternateAction;
   public event EventHandler OnPauseAction;
+
+  public enum Bindings
+    {
+        MoveUp, 
+        MoveDown, 
+        MoveLeft, 
+        MoveRight,
+        Interact,
+        InteractAlternate,
+        Pause,
+    }
+
   private void Awake()
   {
     instance = this;
@@ -21,6 +33,8 @@ public class GameInput : MonoBehaviour
     playerInputActions.Player.Interact.performed += Interact_performed;
     playerInputActions.Player.InteractAlternate.performed += InteractAlternate_performed;
     playerInputActions.Player.Pause.performed += Pause_performed;
+
+    Debug.Log(GetBindingText(Bindings.Interact));   
   }
 
     private void OnDestroy()
@@ -62,4 +76,36 @@ public class GameInput : MonoBehaviour
       OnInteractAction?.Invoke(this, EventArgs.Empty);
     
   }
+
+    public string GetBindingText(Bindings binding)
+    {
+        
+        switch (binding) {
+            default:
+
+            case Bindings.MoveUp:
+                return playerInputActions.Player.Move.bindings[1].ToDisplayString();
+
+            case Bindings.MoveDown:
+                return playerInputActions.Player.Move.bindings[2].ToDisplayString();
+
+            case Bindings.MoveLeft:
+                return playerInputActions.Player.Move.bindings[3].ToDisplayString();
+
+            case Bindings.MoveRight:
+                return playerInputActions.Player.Move.bindings[4].ToDisplayString();
+
+            case Bindings.Interact:
+                return playerInputActions.Player.Interact.bindings[0].ToDisplayString();
+
+            case Bindings.InteractAlternate:
+                return playerInputActions.Player.InteractAlternate.bindings[0].ToDisplayString();
+
+            case Bindings.Pause:
+                return playerInputActions.Player.Pause.bindings[0].ToDisplayString();
+
+            
+
+        }
+    }
 }
--- Assets/Scripts/OptionsUI.cs
+++ Assets/Scripts/OptionsUI.cs
@@ -13,8 +13,24 @@ public class OptionsUI : MonoBehaviour
     [SerializeField] private Button SFXButton;
     [SerializeField] private Button musicButton;
     [SerializeField] private Button closeButton;
+    [SerializeField] private Button moveUpButton;
+    [SerializeField] private Button moveDownButton;
+    [SerializeField] private Button moveLeftButton;
+    [SerializeField] private Button moveRightButton;
+    [SerializeField] private Button interactButton;
+    [SerializeField] private Button interactAltButton;
+    [SerializeField] private Button pauseButton;
+    
+
     [SerializeField] private TextMeshProUGUI sfxText;
     [SerializeField] private TextMeshProUGUI musicText;
+    [SerializeField] private TextMeshProUGUI moveUpText;
+    [SerializeField] private TextMeshProUGUI moveDownText;
+    [SerializeField] private TextMeshProUGUI moveLeftText;
+    [SerializeField] private TextMeshProUGUI moveRightText;
+    [SerializeField] private TextMeshProUGUI interactText;
+    [SerializeField] private TextMeshProUGUI interactAltText;
+    [SerializeField] private TextMeshProUGUI pauseText;
 
     private void Awake()
     {
@@ -50,6 +66,14 @@ public class OptionsUI : MonoBehaviour
     {
         sfxText.text ="Sound effects : " + Mathf.Round(SoundManager.instance.GetVolume()* 10f);
         musicText.text = "Music : " + Mathf.Round(MusicManager.instance.GetVolume() * 10f);
+
+        moveUpText.text = GameInput.instance.GetBindingText(GameInput.Bindings.MoveUp);
+        moveDownText.text = GameInput.instance.GetBindingText(GameInput.Bindings.MoveDown);
+        moveLeftText.text = GameInput.instance.GetBindingText(GameInput.Bindings.MoveLeft);
+        moveRightText.text = GameInput.instance.GetBindingText (GameInput.Bindings.MoveRight);
+        interactText.text = GameInput.instance.GetBindingText(GameInput.Bindings.Interact);
+        interactAltText.text = GameInput.instance.GetBindingText(GameInput.Bindings.InteractAlternate);
+        pauseText.text = GameInput.instance.GetBindingText(GameInput.Bindings .Pause);
     }
 
     public void Show()
