aff2b1c2dafdbf80bd9b93599350daca988a19ca
Bruno Celestino <brunoocelestino@gmail.com>
2025-09-02 10:02:14 -0400

    final.01
    gamedemo completed

--- Assets/Scripts/DeliveryResultUI.cs
+++ Assets/Scripts/DeliveryResultUI.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.Unity.VisualStudio.Editor;
 using System.Collections;
 using System.Collections.Generic;
@@ -19,25 +20,36 @@ public class DeliveryResultUI : MonoBehaviour
     [SerializeField] private Color sucsessColor;
     [SerializeField] private Color failColor;
 
-    
+    private Animator animator;
+    const string PopUpAnimationTrigger = "PopUp";
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     private void Start()
     {
         DeliveyManager.instance.OnRecipeSuccess += DeliveryManager_OnRecipeSuccess;
         DeliveyManager.instance.OnRecipeFailed += DeliveryManager_OnRecipeFailed;
+        gameObject.SetActive(false);
     }
 
     private void DeliveryManager_OnRecipeFailed(object sender, System.EventArgs e)
     {
+        gameObject.SetActive(true);
         iconImage.sprite = failImage;
         backgroundImage.color = failColor;
         message.text = "Delivery\nFailed";
+        animator.SetTrigger(PopUpAnimationTrigger);
     }
 
     private void DeliveryManager_OnRecipeSuccess(object sender, System.EventArgs e)
     {
+        gameObject.SetActive(true);
         iconImage.sprite = sucessImage;
         backgroundImage.color = sucsessColor;
         message.text = "Delivery\nSuccess";
+        animator.SetTrigger(PopUpAnimationTrigger);
     }
 }
